0dba30c37ef303f78810eb05a320924adb484165
Kurt Preston <bitbucket@kurtpreston.com>
2013-08-21 09:23:24 -0500

    Creating empty Rails4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dylome::Application.config.secret_key_base = 'b13f3a220242b26566c5e3c54e25bc22f45c9eb4aae6f3cad32114267f118ef501c9ff572e4ecb959e3b634571309bb0b80b01cf719df9e48923d58d87819645'
