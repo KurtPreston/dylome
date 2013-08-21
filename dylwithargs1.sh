4f3b39f96df0965863b61c7e55489287761bfc23
Kurt Preston <bitbucket@kurtpreston.com>
2013-08-20 23:42:20 -0500

    Formatting script

--- dylwithargs1.sh
+++ dylwithargs1.sh
@@ -1,40 +1,36 @@
 #dylome
 
-
-
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-##echo $SCRIPTPATH    
 
 function float_eval()
 {
-float_scale=3
-local stat=0
-local result=0.0
-if [[ $# -gt 0 ]]; then
-result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
-stat=$?
-if [[ $stat -eq 0  &&  -z "$result" ]]; then stat=1; fi
-fi
-echo $result
-return $stat
+  float_scale=3
+  local stat=0
+  local result=0.0
+  if [[ $# -gt 0 ]]; then
+    result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
+    stat=$?
+    if [[ $stat -eq 0  &&  -z "$result" ]]; then stat=1; fi
+  fi
+  echo $result
+  return $stat
 }
 
 function float_cond()
 {
-local cond=0
-if [[ $# -gt 0 ]]; then
-cond=$(echo "$*" | bc -q 2>/dev/null)
-if [[ -z "$cond" ]]; then cond=0; fi
-if [[ "$cond" != 0  &&  "$cond" != 1 ]]; then cond=0; fi
-fi
-local stat=$((cond == 0))
-return $stat
+  local cond=0
+  if [[ $# -gt 0 ]]; then
+    cond=$(echo "$*" | bc -q 2>/dev/null)
+    if [[ -z "$cond" ]]; then cond=0; fi
+    if [[ "$cond" != 0  &&  "$cond" != 1 ]]; then cond=0; fi
+  fi
+  local stat=$((cond == 0))
+  return $stat
 }
 rm -r $SCRIPTPATH/tempdylome
 rm $SCRIPTPATH/outputfile.mp3
 rm $SCRIPTPATH/piecelist.txt
 mkdir $SCRIPTPATH/tempdylome
-
 
 minutes=0
 mySeconds=0
@@ -45,59 +41,54 @@ complete=0
 displayChunk=$chunk
 cond2="10.0 > $dispsec "
 if float_cond $cond2; then
-displayChunk="0$displayChunk"
+  displayChunk="0$displayChunk"
 fi
-
-
 
 
 #replaces $3
 while [ $complete -le 1 ]
 
 do
-i=$[i+1]
-finame=dylomePiece$[10000-i]
-mySeconds=$(float_eval "$mySeconds + $chunk")
+  i=$[i+1]
+  finame=dylomePiece$[10000-i]
+  mySeconds=$(float_eval "$mySeconds + $chunk")
 
-cond="60.0 <= $mySeconds "
-if float_cond $cond; then
-mySeconds=$(float_eval "$mySeconds - 60.0")
-minutes=$[minutes+1]
+  cond="60.0 <= $mySeconds "
+  if float_cond $cond; then
+    mySeconds=$(float_eval "$mySeconds - 60.0")
+    minutes=$[minutes+1]
 
-fi
+  fi
 
-displyMinutes=$minutes
-if [ 10 -gt $minutes ]; then
-displyMinutes="0$displyMinutes"
-fi
+  displyMinutes=$minutes
+  if [ 10 -gt $minutes ]; then
+    displyMinutes="0$displyMinutes"
+  fi
 
-# doomsday
-if [ $i -gt 1000  ]; then
-complete=10
-fi
+  # doomsday
+  if [ $i -gt 1000  ]; then
+    complete=10
+  fi
 
-dispsec=$mySeconds
-cond2="10.0 > $dispsec "
-if float_cond $cond2; then
-dispsec="0$dispsec"
-fi
-.$SCRIPTPATH/ffmpeg -i $SCRIPTPATH/inputfile.mp3 -vcodec copy -acodec copy -ss 00:$displyMinutes:$dispsec -t 00:00:0$chunk $SCRIPTPATH/tempdylome/$finame.mp3
+  dispsec=$mySeconds
+  cond2="10.0 > $dispsec "
+  if float_cond $cond2; then
+    dispsec="0$dispsec"
+  fi
+  .$SCRIPTPATH/ffmpeg -i $SCRIPTPATH/inputfile.mp3 -vcodec copy -acodec copy -ss 00:$displyMinutes:$dispsec -t 00:00:0$chunk $SCRIPTPATH/tempdylome/$finame.mp3
 
 
-a=$(du -sk $SCRIPTPATH/tempdylome/$finame.mp3 |cut -f 1)
-if [ 5 -gt $a ]; then
-complete=10
-rm $SCRIPTPATH/tempdylome/$finame.mp3
-fi
-
+  a=$(du -sk $SCRIPTPATH/tempdylome/$finame.mp3 |cut -f 1)
+  if [ 5 -gt $a ]; then
+    complete=10
+    rm $SCRIPTPATH/tempdylome/$finame.mp3
+  fi
 done
 
 for f in $SCRIPTPATH/tempdylome/*.mp3
 do
-echo "file '$f'" >> $SCRIPTPATH/piecelist.txt
+  echo "file '$f'" >> $SCRIPTPATH/piecelist.txt
 done
-
-
 
 
 ./$SCRIPTPATH/ffmpeg -f concat -i $SCRIPTPATH/piecelist.txt -c copy $SCRIPTPATH/outputfile.mp3
@@ -105,4 +96,3 @@ done
 rm -r $SCRIPTPATH/tempdylome
 rm $SCRIPTPATH/inputfile.mp3
 rm $SCRIPTPATH/piecelist.txt
-
