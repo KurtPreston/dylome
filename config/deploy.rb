26357932cbd6ab02c4a7e9038f534767ec715ddf
Kurt Preston <bitbucket@kurtpreston.com>
2013-08-29 13:51:16 -0500

    Symlink the database configuration on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,13 +9,21 @@ set :ssh_options, { :forward_agent => true }
 
 server "tefnut", :app, :web, :db, :primary => true
 
+after 'deploy:update_code', 'deploy:symlink_db'
 after "deploy:restart", "deploy:cleanup"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
   task :start do ; end
+
   task :stop do ; end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
 end
