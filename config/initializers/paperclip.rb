330c21f8f6a4bc93bf7ba213f4cdfffa7f3fbd39
Kurt Preston <github@kurtpreston.com>
2016-03-14 11:43:32 -0500

    Fixing paperclip config interpolation

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 # Load paperclip config
-paperclip_config = YAML.load_file("#{Rails.root}/config/paperclip.yml")
+paperclip_config = YAML.load(ERB.new(File.new("#{Rails.root}/config/paperclip.yml").read).result)
 if paperclip_config[Rails.env]
   Paperclip::Attachment.default_options.merge!(paperclip_config[Rails.env])
 end
